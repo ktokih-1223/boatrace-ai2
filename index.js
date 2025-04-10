879f600f4511c15e2253956caeb3ce369b9753e3
ktokih-1223 <ktokih1223@gmail.com>
2025-04-10 12:37:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+// 競艇予想を返すエンドポイント（仮）
+app.get('/predict', (req, res) => {
+  const raceData = {
+    race: '住之江1R',
+    time: '15:00',
+    predictions: {
+      main: '1-23-234',
+      targets: ['1-4-23', '1-5-234', '1-6-流']
+    }
+  };
+  res.json(raceData);
 });
 
 app.listen(port, () => {
